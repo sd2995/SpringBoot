9be361fbea6854dca41dbe1c8450b4353f270572
Shubham Deshmukh <38827214+sd2995@users.noreply.github.com>
2022-07-07 20:40:34 +0530

    Live Reload Example

---
+++ SpringBootLiveRelod/src/test/java/com/example/FirstAPI/FirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
